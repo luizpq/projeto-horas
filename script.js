c228ef3599f0c74f652c50f32f1c18ca72226cd1
luizpq <luizfabiano1007@gmail.com>
2024-08-01 17:18:37 -0300

    Finalizando o projeto

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-var agora = new Date()
-var hora = agora.getHours()
-horas.innerText += `Agora são ${hora} horas`
+function carregar () {
+   var agora = new Date()
+    var hora = agora.getHours()
+    var img = document.getElementById('imagem')
+    
+    horas.innerHTML = `Agora são ${hora} horas`
+    if (hora >= 0 && hora < 12) {
+        img.src = 'foto-manha.jpg'
+        document.body.style.backgroundColor = '#c7ae3ba4'
+    }
+    else if (hora >= 12 && hora < 18) {
+        img.src = "foto-tarde.jpg"
+        document.body.style.backgroundColor = '#bb8013'
+    }
 
+    else{
+        img.src = 'foto-noite.jpg'
+        document.body.style.backgroundColor = '#113591e2'
+    }
+}
